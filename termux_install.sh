45fde1f12ffa147b38fb2d6f32965ca07ce578a2
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-03-04 22:49:09 +0700

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -1,13 +1,12 @@
-
-_get_ziplink () {
+__ziplink () {
     local regex
     regex='(https?)://github.com/.+/.+'
     if [[ $PANDA_USERBOT_REPO == "PANDA_USERBOT" ]]
     then
         echo "aHR0cHM6Ly9naXRodWIuY29tL2lsaGFtbWFuc2l6L1BhbmRhWF9Vc2VyYm90L2FyY2hpdmUvUGFuZGFVc2VyYm90LnppcA==" | base64 -d
-    elif [[ $PANDA_USERBOT_REPO == "PANDA_USERBOT" ]]
+    elif [[ $PANDA_USERBOT_REPO == "BUKAN_USERBOT" ]]
     then
-        echo "aHR0cHM6Ly9naXRodWIuY29tL2lsaGFtbWFuc2l6L1BhbmRhWF9Vc2VyYm90L2FyY2hpdmUvUGFuZGFVc2VyYm90LnppcA==" | base64 -d
+        echo "aHR0cHM6Ly9naXRodWIuY29tL0FmdGFoQmFnYXMvUGFuZGFYX1VzZXJib3QvYXJjaGl2ZS9QYW5kYVVzZXJib3Quemlw" | base64 -d
     elif [[ $PANDA_USERBOT_REPO =~ $regex ]]
     then
         if [[ $PANDA_USERBOT_REPO_BRANCH ]]
@@ -21,11 +20,17 @@ _get_ziplink () {
     fi
 }
 
-_get_repolink () {
+__repolink () {
     local regex
     local rlink
     regex='(https?)://github.com/.+/.+'
     if [[ $UPSTREAM_REPO == "PANDA_USERBOT" ]]
+    then
+        rlink=`echo "aHR0cHM6Ly9naXRodWIuY29tL2lsaGFtbWFuc2l6L1BhbmRhWF9Vc2VyYm90" | base64 -d`
+    elif [[ $UPSTREAM_REPO == "BUKAN_USERBOT" ]]
+    then
+        rlink=`echo "aHR0cHM6Ly9naXRodWIuY29tL0FmdGFoQmFnYXMvUGFuZGFYX1VzZXJib3Q=" | base64 -d`
+    elif [[ $UPSTREAM_REPO =~ $regex ]]
     then
         rlink=`echo "${UPSTREAM_REPO}"`
     else
@@ -35,12 +40,14 @@ _get_repolink () {
 }
 
 
-_run_python_code() {
+
+
+_install_python_version() {
     python3${pVer%.*} -c "$1"
 }
 
-_run_pandapack_git() {
-    $(_run_python_code 'from git import Repo
+_install_deploy_git() {
+    $(_install_python_version 'from git import Repo
 import sys
 OFFICIAL_UPSTREAM_REPO = "https://github.com/ilhammansiz/DEPLOY"
 ACTIVE_BRANCH_NAME = "PandaUserbot"
@@ -51,8 +58,8 @@ repo.create_head(ACTIVE_BRANCH_NAME, origin.refs[ACTIVE_BRANCH_NAME])
 repo.heads[ACTIVE_BRANCH_NAME].checkout(True) ')
 }
 
-_run_panda_git() {
-    local repolink=$(_get_repolink)
+_start_install_git() {
+    local repolink=$(__repolink)
     $(_run_python_code 'from git import Repo
 import sys
 OFFICIAL_UPSTREAM_REPO="'$repolink'"
@@ -65,29 +72,26 @@ repo.heads[ACTIVE_BRANCH_NAME].checkout(True) ')
 }
 
 
-_set_bot () {
+_install_pandauserbot () {
     local zippath
     zippath="pandauserbot.zip"
     echo "  Downloading source code ..."
-    wget -q $(_get_ziplink) -O "$zippath"
+    wget -q $(__ziplink) -O "$zippath"
     echo "  Unpacking Data ..."
     PANDA_USERBOTPATH=$(zipinfo -1 "$zippath" | grep -v "/.");
     unzip -qq "$zippath"
     echo "Done"
     echo "  Cleaning ..."
     rm -rf "$zippath"
-    _run_pandapack_git
+    _install_deploy_git
     cd $PANDA_USERBOTPATH
-    _run_panda_git
-    python3 ../setup/updater.py ../requirements.txt requirements.txt
-    chmod -R 755 bin
+    _start_install_git
     echo "    Starting PandaUserBot    "
     echo "
-                        ((((((PANDA_USERBOT))))
-                         ┏━━━━━━━━━━━━━━━━━━━ 
-                         ┗━━━━━━━━━━━━━━━━━━━ 
+              Panda Userbot
+            
     "
     python3 -m Panda
 }
 
-_set_bot
+_install_pandauserbot
