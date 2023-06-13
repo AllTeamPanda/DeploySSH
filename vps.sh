ff6b647ff347d9a6c61c3a14380cb6ae57e5c850
Panda Userbot Organization <132319106+DevTeamPanda@users.noreply.github.com>
2023-06-13 08:07:05 +0700

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -18,7 +18,7 @@ sudo apt install python3-wand
 sudo apt install python3-lxml
 sudo apt install postgresql-client
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-pip3 install pytgcalls==3.0.0.dev24 && pip3 install av -q --no-binary av
+pip3 install av -q --no-binary av
 
 git clone https://github.com/AllTeamPanda/PandaX_Userbot
 cd PandaX_Userbot
